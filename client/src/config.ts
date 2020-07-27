022040ac1094c000c821ecd31b732fc2ffbb8900
Rebekka Haley <rebekka.haley@gmail.com>
2020-07-27 22:17:43 +0100

    [client] updated apiId (aws) and clientId (auth0)

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // DONE: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'g65iy40yob'
+const apiId = 'zjjz8smjl2'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`
 
 export const authConfig = {
   // DONE: reate an Auth0 application and copy values from it into this map
-  domain: 'dev-eb-o8fgn.eu.auth0.com',            // Auth0 domain
-  clientId: 'l3s9l1W1QsyZwKShHVb001Nk8fNkXaJo',          // Auth0 client id
+  domain: 'dev-eb-o8fgn.eu.auth0.com',  // Auth0 domain
+  clientId: 'OoayT4xG1dbRv1SEUb5qTmCvEtPiD3C3',  // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
